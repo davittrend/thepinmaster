c2254c05ad669cea3fc2d1b3aabbdc25a21d5b53
davittrend <davidtrendovski@gmail.com>
2024-12-01 10:36:23 +0100

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,2 +1,10 @@
-...deleted...
+import { AuthForm } from "@/components/auth-form"
+
+export default function SignInPage() {
+  return (
+    <main className="flex-1 flex items-center justify-center py-16">
+      <AuthForm mode="login" />
+    </main>
+  )
+}
 
